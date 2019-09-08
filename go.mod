bb13327eeb916ca2464ba0d6cd4061b993ab37d9
Zack Scholl <zack.scholl@gmail.com>
2019-09-08 06:24:03 -0700

    improve tests

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/stretchr/testify v1.3.0
 	github.com/tscholl2/siec v0.0.0-20180721101609-21667da05937
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
+	golang.org/x/crypto v0.0.0-20190907121410-71b5226ff739
 )
+
+go 1.13
--- pake_test.go
+++ pake_test.go
@@ -57,231 +57,29 @@ func BenchmarkPakeP224(b *testing.B) {
 	}
 }
 
-func TestPake(t *testing.T) {
-	curve := siec.SIEC255()
-	// successful (both have same k)
-	// initialize A
-	A, err := Init([]byte{1, 2, 3}, 0, curve, 100*time.Millisecond)
-	assert.Nil(t, err)
-	assert.False(t, A.IsVerified())
-	// initialize B
-	B, err := Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	assert.Nil(t, err)
-	assert.False(t, B.IsVerified())
-	// send A's stuff to B
-	err = B.Update(A.Bytes())
-	assert.Nil(t, err)
-	assert.False(t, B.IsVerified())
-	// send B's stuff to A
-	err = A.Update(B.Bytes())
-	assert.Nil(t, err) // A validates
-	assert.True(t, A.IsVerified())
-	// send A's stuff back to B
-	err = B.Update(A.Bytes())
-	assert.Nil(t, err) // B validates
-	assert.True(t, B.IsVerified())
-
-	// failure (both have different k)
-	// initialize A
-	A, err = Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	assert.Nil(t, err)
-	assert.False(t, A.IsVerified())
-	// initialize B
-	B, err = Init([]byte{4, 5, 6}, 1, curve, 1*time.Millisecond)
-	assert.Nil(t, err)
-	assert.False(t, B.IsVerified())
-	// send A's stuff to B
-	err = B.Update(A.Bytes())
-	assert.Nil(t, err)
-	assert.False(t, B.IsVerified())
-	// send B's stuff to A
-	err = A.Update(B.Bytes())
-	assert.NotNil(t, err) // A validates
-	assert.False(t, A.IsVerified())
-	// send A's stuff back to B
-	err = B.Update(A.Bytes())
+func TestError(t *testing.T) {
+	A, err := InitCurve([]byte{1, 2, 3}, 0, "nosuchcurve", 1*time.Millisecond)
 	assert.NotNil(t, err)
-	assert.False(t, B.IsVerified())
-
-}
-
-func TestSessionKeySIEC255(t *testing.T) {
-	curve := siec.SIEC255()
-	// initialize A
-	A, _ := Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	// initialize B
-	B, _ := Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	// send A's stuff to B
-	B.Update(A.Bytes())
-	// send B's stuff to A
-	A.Update(B.Bytes())
-	// send A's stuff back to B
-	B.Update(A.Bytes())
-	s1, err := A.SessionKey()
+	A, err = InitCurve([]byte{1, 2, 3}, 0, "p521")
 	assert.Nil(t, err)
-	s1B, err := B.SessionKey()
+	_, err = A.SessionKey()
+	assert.NotNil(t, err)
+	B, err := InitCurve([]byte{1, 2, 3}, 0, "p521")
 	assert.Nil(t, err)
-	assert.Equal(t, s1, s1B)
+	assert.NotNil(t, B.Update(A.Bytes()))
+	assert.False(t, A.IsVerified())
+	assert.NotNil(t, B.Update([]byte("{1:1}")))
 
-	// initialize A
-	A, _ = Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	// initialize B
-	B, _ = Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	// send A's stuff to B
-	B.Update(A.Bytes())
-	// send B's stuff to A
-	A.Update(B.Bytes())
-	// send A's stuff back to B
-	B.Update(A.Bytes())
-	s2, err := A.SessionKey()
-	assert.Nil(t, err)
-
-	assert.NotEqual(t, s1, s2)
-}
-
-// // THIS TEST DOES NOT WORK!?!
-// func TestSessionKeyP224(t *testing.T) {
-// 	curve := elliptic.P224()
-// 	// initialize A
-// 	A, _ := Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-// 	// initialize B
-// 	B, _ := Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-// 	// send A's stuff to B
-// 	B.Update(A.Bytes())
-// 	// send B's stuff to A
-// 	A.Update(B.Bytes())
-// 	// send A's stuff back to B
-// 	B.Update(A.Bytes())
-// 	s1, err := A.SessionKey()
-// 	assert.Nil(t, err)
-// 	s1B, err := B.SessionKey()
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, s1, s1B)
-
-// 	// initialize A
-// 	A, _ = Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-// 	// initialize B
-// 	B, _ = Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-// 	// send A's stuff to B
-// 	B.Update(A.Bytes())
-// 	// send B's stuff to A
-// 	A.Update(B.Bytes())
-// 	// send A's stuff back to B
-// 	B.Update(A.Bytes())
-// 	s2, err := A.SessionKey()
-// 	assert.Nil(t, err)
-
-// 	assert.NotEqual(t, s1, s2)
-// }
-
-func TestSessionKeyP521(t *testing.T) {
-	curve := elliptic.P521()
-	// initialize A
-	A, _ := Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	// initialize B
-	B, _ := Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	// send A's stuff to B
-	B.Update(A.Bytes())
-	// send B's stuff to A
-	A.Update(B.Bytes())
-	// send A's stuff back to B
-	B.Update(A.Bytes())
-	s1, err := A.SessionKey()
-	assert.Nil(t, err)
-	s1B, err := B.SessionKey()
-	assert.Nil(t, err)
-	assert.Equal(t, s1, s1B)
-
-	// initialize A
-	A, _ = Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	// initialize B
-	B, _ = Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	// send A's stuff to B
-	B.Update(A.Bytes())
-	// send B's stuff to A
-	A.Update(B.Bytes())
-	// send A's stuff back to B
-	B.Update(A.Bytes())
-	s2, err := A.SessionKey()
-	assert.Nil(t, err)
-
-	assert.NotEqual(t, s1, s2)
-}
-
-func TestSessionKeyP256(t *testing.T) {
-	curve := elliptic.P256()
-	// initialize A
-	A, _ := Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	// initialize B
-	B, _ := Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	// send A's stuff to B
-	B.Update(A.Bytes())
-	// send B's stuff to A
-	A.Update(B.Bytes())
-	// send A's stuff back to B
-	B.Update(A.Bytes())
-	s1, err := A.SessionKey()
-	assert.Nil(t, err)
-	s1B, err := B.SessionKey()
-	assert.Nil(t, err)
-	assert.Equal(t, s1, s1B)
-
-	// initialize A
-	A, _ = Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	// initialize B
-	B, _ = Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	// send A's stuff to B
-	B.Update(A.Bytes())
-	// send B's stuff to A
-	A.Update(B.Bytes())
-	// send A's stuff back to B
-	B.Update(A.Bytes())
-	s2, err := A.SessionKey()
-	assert.Nil(t, err)
-
-	assert.NotEqual(t, s1, s2)
-}
-
-func TestSessionKeyP384(t *testing.T) {
-	curve := elliptic.P384()
-	// initialize A
-	A, _ := Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	// initialize B
-	B, _ := Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	// send A's stuff to B
-	B.Update(A.Bytes())
-	// send B's stuff to A
-	A.Update(B.Bytes())
-	// send A's stuff back to B
-	B.Update(A.Bytes())
-	s1, err := A.SessionKey()
-	assert.Nil(t, err)
-	s1B, err := B.SessionKey()
-	assert.Nil(t, err)
-	assert.Equal(t, s1, s1B)
-
-	// initialize A
-	A, _ = Init([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
-	// initialize B
-	B, _ = Init([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
-	// send A's stuff to B
-	B.Update(A.Bytes())
-	// send B's stuff to A
-	A.Update(B.Bytes())
-	// send A's stuff back to B
-	B.Update(A.Bytes())
-	s2, err := A.SessionKey()
-	assert.Nil(t, err)
-
-	assert.NotEqual(t, s1, s2)
 }
 
 func TestSessionKeyString(t *testing.T) {
 	curves := []string{"siec", "p384", "p521", "p256"}
 	for _, curve := range curves {
-		A, _ := InitCurve([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
+		A, err := InitCurve([]byte{1, 2, 3}, 0, curve, 1*time.Millisecond)
+		assert.Nil(t, err)
 		// initialize B
-		B, _ := InitCurve([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
+		B, err := InitCurve([]byte{1, 2, 3}, 1, curve, 1*time.Millisecond)
+		assert.Nil(t, err)
 		// send A's stuff to B
 		B.Update(A.Bytes())
 		// send B's stuff to A
@@ -308,5 +106,7 @@ func TestSessionKeyString(t *testing.T) {
 		assert.Nil(t, err)
 
 		assert.NotEqual(t, s1, s2)
+		assert.True(t, A.IsVerified())
+		assert.True(t, B.IsVerified())
 	}
 }
